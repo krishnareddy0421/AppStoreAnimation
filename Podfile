869112e33ac7b4195e3fd40ca8b0942a79022c10
vamsi krishna reddy kamjula <krishnareddy0421@gmail.com>
2017-12-12 12:59:10 -0800

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+
+target 'RealmSwift' do
+  use_frameworks!
+  
+  pod 'RealmSwift', '>= 0.92.3'
+end
--- RealmSwift/ViewController.swift
+++ RealmSwift/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath)
+        
+        return cell
+    }
 }
 
